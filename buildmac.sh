e4718e6a6c6874bfc3149f2918cf8bb3c311b01f
a.cianfarani <a.cianfarani@c2is.fr>
2016-07-06 10:14:56 +0200

    Dmg macosx

--- buildmac.sh
+++ buildmac.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 go build -o macosx/c2rprox/c2rprox c2rprox.go
 /Applications/xcode_auxiliary_tools/PackageMaker.app/Contents/MacOS/PackageMaker -d ./macosx/c2rprox.pmdoc -o ./macosx/c2rprox.pkg
+
+mkdir ./macosx/tmp
+cp -R ./macosx/c2rprox.pkg ./macosx/tmp
+hdiutil create -srcfolder "/Users/andre/Documents/Work/go/c2rprox/macosx/tmp/" -volname "C2rproxInstall" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW -size 13m ./macosx/C2rproxInstall.dmg
+rm -rf ./macosx/tmp
