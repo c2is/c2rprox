ce1882a96046695b6e3b8646230e7c72cd315dce
a.cianfarani <a.cianfarani@c2is.fr>
2016-09-21 17:33:47 +0200

    Fix launchd environment

--- macosx/c2rprox/postflight.sh
+++ macosx/c2rprox/postflight.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+chmod +x /Library/LaunchDaemons/fr.c2is.c2rprox.plist
+chown root:admin /Library/LaunchDaemons/fr.c2is.c2rprox.plist
 ln -nfs /usr/share/c2rprox/LaunchDaemons/fr.c2is.c2rprox.plist /Library/LaunchDaemons/fr.c2is.c2rprox.plist
 ln -nfs /usr/share/c2rprox/c2rproxd /usr/bin/c2rproxd
 ln -nfs /usr/share/c2rprox/targets /etc/targets
